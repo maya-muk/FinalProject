238fa8ee38e91dd71d46740bdb3991c0038a3525
maya muk <mukbilmaya@gmail.com>
2023-03-11 11:25:12 +0300

    mayaUpdate

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactusComponent } from './contactus/contactus.component';
 import { IndexComponent } from './index/index.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
 
 const routes: Routes = [
   {
     path:"main",
     component:IndexComponent
+  },
+  {
+    path:"about",
+    component:AboutComponent
+  },
+  {
+    path:"contact",
+    component:ContactusComponent
+  },
+  {
+    path:"testimonial",
+    component:TestimonialComponent
   }
 ];
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,11 +4,17 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { IndexComponent } from './index/index.component';
 import { SharedModule } from '../shared/shared.module';
+import { AboutComponent } from './about/about.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
 
 
 @NgModule({
   declarations: [
-    IndexComponent
+    IndexComponent,
+    AboutComponent,
+    ContactusComponent,
+    TestimonialComponent
   ],
   imports: [
     CommonModule,
