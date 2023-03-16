733565600929fb21a42b0bddcb29dd8a066546d8
kdiabb <125552895+kdiabb@users.noreply.github.com>
2023-03-16 13:19:57 +0300

    remove tescor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule } from 'ngx-toastr';
+
 import { HttpClientModule } from '@angular/common/http'; //hit on api
 
 
@@ -16,8 +16,7 @@ import { HttpClientModule } from '@angular/common/http'; //hit on api
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule,
-    ToastrModule.forRoot()
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
