f7392b0ccb331718ab150d19d2cb9167fb8fa5c3
kdiabb <125552895+kdiabb@users.noreply.github.com>
2023-03-19 16:56:33 +0300

    khaled

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -21,7 +21,7 @@ RegisterForm = new FormGroup(
   {
     Username : new FormControl('',[Validators.required]),
     Email: new FormControl('',[Validators.required , Validators.email]),
-    Password : new FormControl('',[Validators.required , Validators.minLength(5)]),
+    Password : new FormControl('',[Validators.required , Validators.minLength(5),Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')]),
     Firstname : new FormControl('',[Validators.required]),
     Lastname : new FormControl('',[Validators.required])
   }
