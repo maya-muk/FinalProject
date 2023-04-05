97b52851e20462db93ed305ddc0b93d8d533191e
kdiabb <125552895+kdiabb@users.noreply.github.com>
2023-04-05 15:22:15 +0300

    kk

--- src/app/admin/managecontact/managecontact.component.ts
+++ src/app/admin/managecontact/managecontact.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { AdminService } from 'src/app/admin.service';
 
 @Component({
   selector: 'app-managecontact',
@@ -7,4 +9,86 @@ import { Component } from '@angular/core';
 })
 export class ManagecontactComponent {
 
+
+  constructor(public adminService: AdminService) { }
+
+
+Updateabout = new FormGroup(
+  {
+    contactdyid: new FormControl(''),
+    contactbackimage: new FormControl(''),
+    contactbackword: new FormControl(''),
+    contactformword: new FormControl(''),
+    contactformemail: new FormControl(''),
+    contactformphone: new FormControl(''),
+    contactformadress: new FormControl('')
+
 }
+ 
+  
+  )
+  about:any
+  async ngOnInit(){
+    await this.adminService.GetAllAbout()
+    
+    await this.ondata()
+  }
+  
+async ondata(){
+
+  this.about =  this.adminService.AllAbout[0]
+  this.about=this.about[0]
+  console.log(this.about)
+
+   this.Updateabout.patchValue(this.adminService.AllAbout[0])
+   console.log(this.adminService.AllAbout[0])
+ }
+
+  
+  UploadImage1(input: any) // <input>
+  {
+    console.log(input);
+    console.log(input.files);
+    if (input.files.length != 0) {
+      let uploadedFile = input.files[0] // imagefile 
+      let formData = new FormData()
+      formData.append('file', uploadedFile)
+      this.adminService.UploadImage1(formData)
+    }
+
+  }
+  
+  UploadImage2(input: any) // <input>
+  {
+    console.log(input);
+    console.log(input.files);
+    if (input.files.length != 0) {
+      let uploadedFile = input.files[0] // imagefile 
+      let formData = new FormData()
+      formData.append('file', uploadedFile)
+      this.adminService.UploadImage2(formData)
+    }
+
+  }
+  
+  UploadImage3(input: any) // <input>
+  {
+    console.log(input);
+    console.log(input.files);
+    if (input.files.length != 0) {
+      let uploadedFile = input.files[0] // imagefile 
+      let formData = new FormData()
+      formData.append('file', uploadedFile)
+      this.adminService.UploadImage3(formData)
+    }
+
+  }
+ 
+  updateinfo(){
+   this.adminService.UpdateAbout(this.Updateabout.value)
+   this.adminService.GetAllAbout()
+  }
+
+}
+
+
